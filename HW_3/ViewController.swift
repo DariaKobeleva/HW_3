c3662e09988b0e62fbb65d408bea8acfe78a5dde
KDaria <dkdva@mail.ru>
2024-02-02 10:42:58 +0100

    Initial Commit

---
+++ HW_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_3
+//
+//  Created by Дарья Кобелева on 02.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
