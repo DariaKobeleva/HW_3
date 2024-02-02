4008f4c91e8aa09bad79d8cb295ad32726009bb5
KDaria <dkdva@mail.ru>
2024-02-02 13:32:36 +0100

    Implemented logic for displaying colors in a view using RGB sliders.

--- HW_3/ViewController.swift
+++ HW_3/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+final class ViewController: UIViewController {
+
+    @IBOutlet var colorMixView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greeenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorMixView.layer.cornerRadius = 12
     }
 
-
+    @IBAction func sliderValueChanged() {
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        greeenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        
+        colorMixView.backgroundColor = UIColor(
+            red: CGFloat(
+                redSlider.value
+            ),
+            green: CGFloat(
+                greenSlider.value
+            ),
+            blue: CGFloat(
+                blueSlider.value
+            ),
+            alpha: 1
+        )
+    }
+    
+    
 }
 
